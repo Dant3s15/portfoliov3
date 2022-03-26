c7d2deecc31f727048bbc086d25485ecae46d699
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-03-26 15:23:41 +0100

    Added swipe gestures on characters

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -3,19 +3,6 @@ import ReactDOM from 'react-dom';
 import Character from './Character';
 
 const Characters = props => {
-  const calcCharWidth = () => {
-    let characters = document.querySelector('.characters');
-    let characterImgComputedWidth = parseInt(
-      window.getComputedStyle(characters).width
-    );
-    let charactersComputedWith = characterImgComputedWidth * 0.4;
-
-    let root = document.documentElement;
-    root.style.setProperty('--characters-width', charactersComputedWith + 'px');
-  };
-  calcCharWidth();
-  window.addEventListener('resize', calcCharWidth);
-
   const [leftChar, setLeftChar] = useState(0);
   const [frontChar, setFrontChar] = useState(1);
   const [rightChar, setRightChar] = useState(2);
@@ -29,8 +16,25 @@ const Characters = props => {
     props.onCharStateChange(charStateData);
   }, [leftChar, frontChar, rightChar]);
 
+  // ********************************************************
+  const calcCharWidth = () => {
+    let characters = document.querySelector('.characters');
+    let characterImgComputedWidth = parseInt(
+      window.getComputedStyle(characters).width
+    );
+    let charactersComputedWith = characterImgComputedWidth * 0.4;
+
+    let root = document.documentElement;
+    root.style.setProperty('--characters-width', charactersComputedWith + 'px');
+  };
+
+  calcCharWidth();
+
+  window.addEventListener('resize', calcCharWidth);
+
+  // ********************************************************
+
   const wrapRotate = function (num, direc = -1) {
-    // console.log(CharDataCtx);
     if (direc == 1) {
       return (num + 1) % 3;
     }
@@ -43,7 +47,9 @@ const Characters = props => {
     }
   };
 
-  const rotateCharactersHandler = e => {
+  // *****************************************************
+
+  const rotateCharactersHandler = (e, gesture) => {
     const setChars = direc => {
       setLeftChar(wrapRotate(leftChar, direc));
       setFrontChar(wrapRotate(frontChar, direc));
@@ -51,48 +57,81 @@ const Characters = props => {
     };
 
     let direc = 0;
+    let charData;
+    if (!e === undefined) {
+      charData = +e.target.dataset.character;
+    } else charData = gesture;
 
     //TODO if===1
-    if (+e.target.dataset.character === 1) {
+    if (charData === 1) {
       console.log('FRONT TODO');
     }
 
-    if (+e.target.dataset.character === 0) {
+    if (charData === 0) {
       direc = 1;
       setChars(direc);
     }
-    if (+e.target.dataset.character === 2) {
+    if (charData === 2) {
       direc = -1;
       setChars(direc);
     }
   };
+  // *****************************************************
   //TODO handle swipe gestures
   let touchstartX = 0;
   let touchendX = 0;
 
-  function handleGesture() {
-    if (Math.abs(touchendX - touchstartX) > 50) {
-      if (touchendX < touchstartX) {
-        rotateRight();
-      }
-      if (touchendX > touchstartX) {
-        rotateLeft();
-      }
-    }
+  function debounce(func, timeout = 3000) {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, timeout);
+    };
   }
+
+  // function checkSwipeDirection() {
+  //   if (Math.abs(touchendX - touchstartX) > 50) {
+  //     if (touchendX < touchstartX) {
+  //       // rotateCharactersHandler();
+  //       rotateCharactersHandler(null, 2);
+  //       // console.log('rotate right');
+  //     }
+  //     if (touchendX > touchstartX) {
+  //       // rotateLeft();
+  //       rotateCharactersHandler(null, 0);
+  //       // console.log('rotate left');
+  //     }
+  //   }
+  // }
+
+  // function handleGesture() {
+  //   // console.log('handling');
+  //   if (touchendX < touchstartX) rotateCharactersHandler(null, 0);
+  //   if (touchendX > touchstartX) rotateCharactersHandler(null, 2);
+  // }
+  //TODO fix swipe endless loop
   const charactersEl = document.querySelector('.characters');
-
-  charactersEl.addEventListener('touchstart', e => {
-    touchstartX = e.changedTouches[0].screenX;
+  charactersEl.addEventListener('swiped', function (e) {
+    // console.log(e.target);
+    if (e.detail.dir === 'right') rotateCharactersHandler(null, 0);
+    if (e.detail.dir === 'left') rotateCharactersHandler(null, 2);
   });
 
-  charactersEl.addEventListener('touchend', e => {
-    touchendX = e.changedTouches[0].screenX;
+  // charactersEl.addEventListener('touchstart', e => {
+  //   touchstartX = e.changedTouches[0].screenX;
+  // });
 
-    handleGesture();
-  });
+  // charactersEl.addEventListener('touchend', e => {
+  //   touchendX = e.changedTouches[0].screenX;
+  //   handleGesture();
+  // });
+
+  // *****************************************************
 
   const charactersRoot = document.getElementById('characters-root');
+
   return (
     <Fragment>
       {ReactDOM.createPortal(
