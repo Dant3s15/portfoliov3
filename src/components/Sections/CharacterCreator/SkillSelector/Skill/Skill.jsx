dc461106598356c1dee6f9504e646b987aaf4cc2
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-04-05 19:33:29 +0200

    CSS - Fixed icon size inside Skill

--- src/components/Sections/CharacterCreator/SkillSelector/Skill/Skill.jsx
+++ src/components/Sections/CharacterCreator/SkillSelector/Skill/Skill.jsx
@@ -3,7 +3,9 @@ import classes from './Skill.module.scss';
 const Skill = props => {
   return (
     <div className={classes.skill}>
-      <div className={classes.icon}>SVG</div>
+      <div className={classes.icon}>
+        <img src={props.data.icon} alt='skill icon' />
+      </div>
       <div className={classes.name}>
         {props.data.name} {props.data.id}
       </div>
