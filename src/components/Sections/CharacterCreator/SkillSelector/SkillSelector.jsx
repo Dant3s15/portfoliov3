3db85f1d60071cc66b72baa3973e4b40d62b4e88
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-04-05 20:50:31 +0200

    REACT - Added filtering in All skills column inside SkillSelector

--- src/components/Sections/CharacterCreator/SkillSelector/SkillSelector.jsx
+++ src/components/Sections/CharacterCreator/SkillSelector/SkillSelector.jsx
@@ -1,23 +1,74 @@
-import { Fragment } from 'react';
+import { Fragment, useRef, useState, useEffect } from 'react';
 import CardGlass from '../../../UI/CardGlass';
 import AllSkills from '../../../Utils/AllSkills';
 import Skill from './Skill/Skill';
 import classes from './SkillSelector.module.scss';
 
 const SkillSelector = props => {
+  const [searchAllValue, setSearchAllValue] = useState(AllSkills);
+
+  const searchAllRef = useRef('');
+
+  useEffect(() => {
+    filterAllSkills();
+  }, []);
+
+  const filterAllSkills = () => {
+    setSearchAllValue(
+      AllSkills.filter(skill => {
+        if (skill.name) {
+          if (skill.name.toLowerCase().includes(searchAllRef.current.value)) {
+            return <Skill key={skill.id} data={skill}></Skill>;
+          }
+        }
+      })
+    );
+  };
+  // const filterAllSkills = () => {
+  //   console.log(searchAllValue);
+  //   return searchAllValue.map(skill => {
+  //     if (skill.name.toLowerCase().includes(searchAllRef.current.value)) {
+  //       console.log(skill.name);
+  //       return <Skill key={skill.id} data={skill}></Skill>;
+  //     }
+  //   });
+  // };
+
   return (
     <Fragment>
       <div className={classes['skill-selector']}>
         <CardGlass>
           <div className={classes['skills-selector__grid']}>
-            <div className={classes.title}>All Skills</div>
-            <div className={classes.title}>Added Skills</div>
+            <div className={classes['title-row']}>
+              All Skills
+              <div className={classes['search-field']}>
+                <label htmlFor='search-all'>Search</label>
+                <input
+                  onChange={filterAllSkills}
+                  ref={searchAllRef}
+                  id='search-all'
+                  type='text'
+                />
+              </div>
+            </div>
+            <div className={classes['title-row']}>
+              Added Skills
+              <div className={classes['search-field']}>
+                <label htmlFor='search-added'>Search</label>
+                <input id='search-added' type='text' />
+              </div>
+            </div>
             <div className={classes['skills-menu']}>
-              {AllSkills.map(skill => {
-                {
-                  /* console.log(skill.name); */
+              {searchAllValue.map(skill => {
+                if (skill.name) {
+                  if (
+                    skill.name
+                      .toLowerCase()
+                      .includes(searchAllRef.current.value)
+                  ) {
+                    return <Skill key={skill.id} data={skill}></Skill>;
+                  }
                 }
-                return <Skill key={skill.id} data={skill}></Skill>;
               })}
             </div>
             <div className={classes['skills-menu']}>Added skills list</div>
