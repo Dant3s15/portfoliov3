61b56da53c03b515b8b85bf2b10d75785490aeb6
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-04-30 19:41:33 +0200

    CSS - Finished footer layout

--- src/components/Sections/Footer/Footer.tsx
+++ src/components/Sections/Footer/Footer.tsx
@@ -1,13 +1,40 @@
+import github from '../../../resources/icons/logo-github.svg';
+import linkedin from '../../../resources/icons/icon-linkedin-dark.svg';
 import classes from './Footer.module.scss';
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <div>LOGO</div>
-      <div>Github</div>
-      <div>LinkedIn</div>
-      <div>email</div>
+      <div className={classes.wrapper}>
+        <div className={classes.col}>
+          <img src='' alt='Logo' />
+        </div>
+        <div className={classes.col}>
+          <p className={classes.title}>Links:</p>
+          <div>
+            <div className={classes.link}>
+              <img src={github} alt='github' />
+              <a href='https://github.com/Dant3s15'>Github</a>
+            </div>
+            <div className={classes.link}>
+              <img src={linkedin} alt='linkedin' />
+              <a href='#'>LinkedIn</a>
+            </div>
+          </div>
+        </div>
+        <div className={classes.col}>
+          <p className={classes.title}>Contact:</p>
+          <div>
+            <a href='mailto:damiansobierajdev@gmail.com'>
+              damiansobierajdev@gmail.com
+            </a>
+            {/* <p>phone</p> */}
+          </div>
+        </div>
+      </div>
 
-      <div>© 2022 Damian Sobieraj. All Rights Reserved</div>
+      <div className={classes.copyright}>
+        © 2022 Damian Sobieraj. All Rights Reserved
+      </div>
     </footer>
   );
 };
