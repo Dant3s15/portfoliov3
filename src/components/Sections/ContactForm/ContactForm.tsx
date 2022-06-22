ead190999f23d4de69821258a751af9e20f81256
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-06-22 17:50:33 +0200

    REACT - Refactored ContactForm

--- src/components/Sections/ContactForm/ContactForm.tsx
+++ src/components/Sections/ContactForm/ContactForm.tsx
@@ -1,182 +1,151 @@
-import { FormikValues, useFormik, FormikErrors } from 'formik';
-import classes from './ContactForm.module.scss';
-import ButtonBig from '../../UI/ButtonBig';
-import { useState } from 'react';
-// import Email from '../../Utils/smtp.js';
-
-//TODO send email
-// const sendEmail = (values) => {
-//   Email.send({
-//     Host: 'smtp.gmail.com',
-//     Username: 'portfoliod3s@gmail.com',
-//     Password: 'ZAQ!2wsx',
-//     To: 'damiansobierajdev@gmail.com',
-//     From: values.email,
-//     Subject: 'Contact from Portfolio',
-//     Body: `${values.email} ${values.text}`,
-//   }).then(message => alert(message));
-// };
+import {
+  Form,
+  Formik,
+  Field,
+  ErrorMessage,
+  FormikErrors,
+  FormikTouched,
+} from "formik";
+import classes from "./ContactForm.module.scss";
+import ButtonBig from "../../UI/ButtonBig";
 
 const ContactForm = () => {
-  const [formHasError, setFormHasError] = useState(true);
-  const validate = (values: FormikValues) => {
-    const errors: FormikErrors<any> = {};
-
-    if (!values.name) {
-      errors.name = 'Required';
-    } else if (values.name.length < 2) {
-      errors.name = '> 1 characters';
-    }
-
-    if (!values.text) {
-      errors.text = 'Required';
-    } else if (values.text.length < 2) {
-      errors.text = '> 2 characters';
-    }
-
-    if (!values.email) {
-      errors.email = 'Required';
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-    ) {
-      errors.email = 'Invalid email';
-    }
-    if (Object.keys(errors).length === 0) {
-      setFormHasError(false);
-    } else {
-      setFormHasError(true);
-    }
-    return errors;
-  };
-
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      text: '',
-    },
-    validate,
-    onSubmit: async values => {
-      // sendEmail(values);
-      const response = await fetch(
-        'https://portfolio-27cdd-default-rtdb.europe-west1.firebasedatabase.app/emails.json',
-        {
-          method: 'POST',
-          body: JSON.stringify(values, null, 2),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-      const data = await response.json();
-      console.log(data);
-    },
-  });
-
-  const checkErrors = (
-    inptName: 'name' | 'email' | 'text'
-  ): JSX.Element | string | null => {
-    if (formik.errors[inptName] && formik.touched[inptName]) {
-      const errorDiv = (
-        <div className={classes['form-error']}>{formik.errors[inptName]}</div>
-      );
-      return errorDiv;
-    }
-    return null;
-  };
-
-  function checkErrorState(inptName: 'name' | 'email' | 'text') {
-    if (formik.errors[inptName] && formik.touched[inptName]) {
-      return classes['input-error'];
-    } else return undefined;
-  }
-
-  const checkCorner = (inptName: 'name' | 'email' | 'text') => {
-    if (formik.errors[inptName] && formik.touched[inptName])
-      return classes['error-corner'];
-    else return null;
-  };
-
-  const checkError = (inptName: 'name' | 'email' | 'text') => {
-    if (formik.errors[inptName] && formik.touched[inptName])
-      return classes['label--error'];
-    else return null;
+  const checkErrorsAndTouched = (
+    errors: FormikErrors<{ name: string; email: string; text: string }>,
+    touched: FormikTouched<{ name: string; email: string; text: string }>
+  ) => {
+    return (
+      Object.keys(errors).length > 0 ||
+      !touched.name ||
+      !touched.email ||
+      !touched.text
+    );
   };
 
   return (
-    <div id='contact-me' className={classes['contact-me']}>
+    <div id="contact-me" className={classes["contact-me"]}>
       <div className={classes.container}>
-        {/* <form onSubmit={e => e.preventDefault()}> */}
-        <form onSubmit={formik.handleSubmit}>
-          <div className={classes['name-email']}>
-            <div className={`${classes.name} ${checkCorner('name')}`}>
-              <div className={`${classes.labels}`}>
-                <label
-                  className={`${classes.label} ${checkError('name')}`}
-                  htmlFor='name'
-                >
-                  Your Name
-                </label>
-                {checkErrors('name')}
+        <Formik
+          initialValues={{ name: "", email: "", text: "" }}
+          validate={(values) => {
+            const errors: { [key: string]: string } = {};
+            if (!values.name) {
+              errors.name = "Required";
+            } else if (values.name.length < 2) {
+              errors.name = "> 1 characters";
+            }
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email";
+            }
+            if (!values.text) {
+              errors.text = "Required";
+            } else if (values.text.length < 2) {
+              errors.text = "> 2 characters";
+            }
+            return errors;
+          }}
+          onSubmit={async (values, { setSubmitting }) => {
+            const response = await fetch(
+              "https://portfolio-27cdd-default-rtdb.europe-west1.firebasedatabase.app/emails.json",
+              {
+                method: "POST",
+                body: JSON.stringify(values, null, 2),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }
+            );
+            const data = await response.json();
+            console.log(data);
+          }}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <div className={classes["name-email"]}>
+                <div className={classes.name}>
+                  <div className={`${classes.labels}`}>
+                    <label className={classes.label} htmlFor="name">
+                      Your Name
+                    </label>
+                  </div>
+                  <Field
+                    className={`${classes.input} 
+                    ${errors.name && touched.name && classes["input-error"]}
+                  `}
+                    id="name"
+                    type="text"
+                    autoComplete="off"
+                    name="name"
+                  />
+                  <ErrorMessage
+                    name="name"
+                    render={(msg) => (
+                      <div className={classes["form-error"]}>{msg}</div>
+                    )}
+                  />
+                </div>
+                <div className={classes.email}>
+                  <div className={classes.labels}>
+                    <label className={classes.label} htmlFor="email">
+                      Your Email
+                    </label>
+                  </div>
+                  <Field
+                    className={`${classes.input} ${
+                      errors.email && touched.email && classes["input-error"]
+                    }`}
+                    id="email"
+                    type="email"
+                    name="email"
+                    autoComplete="off"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    render={(msg) => (
+                      <div className={classes["form-error"]}>{msg}</div>
+                    )}
+                  />
+                </div>
               </div>
-              <input
-                className={`${classes.input} ${checkErrorState('name')}`}
-                id='name'
-                type='text'
-                autoComplete='off'
-                name='name'
-                onBlur={formik.handleBlur}
-                onChange={formik.handleChange}
-                value={formik.values.name}
-              />
-            </div>
-            <div className={`${classes.email} ${checkCorner('email')}`}>
-              <div className={classes.labels}>
-                <label
-                  className={`${classes.label} ${checkError('email')}`}
-                  htmlFor='email'
-                >
-                  Your Email
-                </label>
-                {checkErrors('email')}
+              <div className={`${classes.textfield}`}>
+                <div className={classes.labels}>
+                  <label className={classes.label} htmlFor="text">
+                    Your Message
+                  </label>
+                </div>
+                <Field
+                  as="textarea"
+                  className={
+                    errors.text && touched.text && classes["input-error"]
+                  }
+                  id="text"
+                  name="text"
+                />
+                <ErrorMessage
+                  className={classes[".form-error "]}
+                  name="text"
+                  render={(msg) => (
+                    <div className={classes["form-error"]}>{msg}</div>
+                  )}
+                />
               </div>
-              <input
-                className={`${classes.input} ${checkErrorState('email')}`}
-                id='email'
-                type='email'
-                name='email'
-                autoComplete='off'
-                onBlur={formik.handleBlur}
-                onChange={formik.handleChange}
-                value={formik.values.email}
+
+              <ButtonBig
+                isGreyedOut={
+                  checkErrorsAndTouched(errors, touched) ? true : false
+                }
+                type={
+                  checkErrorsAndTouched(errors, touched) ? "button" : "submit"
+                }
+                text="Send"
               />
-            </div>
-          </div>
-          <div className={`${classes.textfield}`}>
-            <div className={classes.labels}>
-              <label
-                className={`${classes.label} ${checkError('text')}`}
-                htmlFor='text'
-              >
-                Your Message
-              </label>
-              {checkErrors('text')}
-            </div>
-            <textarea
-              className={checkErrorState('text')}
-              id='text'
-              name='text'
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-              value={formik.values.text}
-            />
-          </div>
-          <ButtonBig
-            isGreyedOut={formHasError ? true : false}
-            type={formHasError ? 'button' : 'submit'}
-            text='Send'
-          ></ButtonBig>
-        </form>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
--- src/components/UI/ButtonBig.tsx
+++ src/components/UI/ButtonBig.tsx
@@ -1,12 +1,12 @@
 // import 'animate.css';
-import { CSSProperties, FC, MouseEventHandler } from 'react';
-import { Link } from 'react-router-dom';
-import classes from './ButtonBig.module.scss';
+import { CSSProperties, FC, MouseEventHandler } from "react";
+import { Link } from "react-router-dom";
+import classes from "./ButtonBig.module.scss";
 
 interface Props {
   style?: CSSProperties | undefined;
   onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
-  type?: 'button' | 'submit' | 'reset' | undefined;
+  type?: "button" | "submit" | "reset" | undefined;
   isAbsolute?: boolean;
   isGreyedOut?: boolean;
   moveTo?: string;
@@ -18,22 +18,22 @@ const ButtonBig: FC<Props> = (props: any) => {
     <button
       type={props.type ? props.type : undefined}
       key={Math.random()}
-      className={`${classes['button-big']}
-    ${props.isAbsolute && props.isAbsolute ? classes.absolute : ''}  ${
-        props.isGreyedOut === true ? classes['greyed-out'] : ''
+      className={`${classes["button-big"]}
+    ${props.isAbsolute && props.isAbsolute ? classes.absolute : ""}  ${
+        props.isGreyedOut === true ? classes["greyed-out"] : ""
       } `}
       style={props.style}
       onClick={props.isGreyedOut ? null : props.onClick}
     >
-      {props.type !== 'submit' && props.iGreyedOut ? (
+      {props.type !== "submit" && props.iGreyedOut ? (
         <Link
-          className={classes['button-text']}
-          to={`${props.moveTo ? props.moveTo : ''}`}
+          className={classes["button-text"]}
+          to={`${props.moveTo ? props.moveTo : ""}`}
         >
           {props.text}
         </Link>
       ) : (
-        <p className={classes['button-text']}>{props.text}</p>
+        <p className={classes["button-text"]}>{props.text}</p>
       )}
     </button>
   );
