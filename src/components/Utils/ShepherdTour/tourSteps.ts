2506f4b4a5e259298bfc2c3e88df90a5a0806472
Damian Sobieraj <damiansobieraj5@gmail.com>
2022-07-24 15:39:40 +0200

    Shepherd - shortened onboarding

--- src/components/Utils/ShepherdTour/tourSteps.ts
+++ src/components/Utils/ShepherdTour/tourSteps.ts
@@ -16,48 +16,8 @@ const tourSteps = [
       enabled: true,
     },
     title: "Hey!",
-    text: ["I'm Damian.<br/> Frontend Web Developer from Poland. "],
-    when: {
-      show: () => {
-        console.log("show step");
-      },
-      hide: () => {
-        console.log("hide step");
-      },
-    },
-    buttons: [
-      {
-        classes: "max-index button-primary",
-        text: "Back",
-        type: "back",
-      },
-      {
-        classes: "max-index button-primary",
-        text: "Next",
-        type: "next",
-      },
-    ],
-  },
-  {
-    id: "2nd",
-    attachTo: { element: "#tourStart", on: "auto" as const },
-    beforeShowPromise: function () {
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          window.scrollTo(0, 0);
-          resolve(true);
-        }, 500);
-      });
-    },
-
-    highlightClass: "highlight",
-    scrollTo: false,
-    cancelIcon: {
-      enabled: true,
-    },
-    title: "Welcome!",
     text: [
-      "This my Portfolio Website.<br/>First time here?<br/> Let me give You a tour! 😄",
+      "I'm Damian.<br/> Frontend Web Developer from Poland and This my Portfolio Website.<br/>First time here?<br/> Let me give You a tour! 😄",
     ],
     when: {
       show: () => {
@@ -80,6 +40,48 @@ const tourSteps = [
       },
     ],
   },
+  // {
+  //   id: "2nd",
+  //   attachTo: { element: "#tourStart", on: "auto" as const },
+  //   beforeShowPromise: function () {
+  //     return new Promise(function (resolve) {
+  //       setTimeout(function () {
+  //         window.scrollTo(0, 0);
+  //         resolve(true);
+  //       }, 500);
+  //     });
+  //   },
+
+  //   highlightClass: "highlight",
+  //   scrollTo: false,
+  //   cancelIcon: {
+  //     enabled: true,
+  //   },
+  //   title: "Welcome!",
+  //   text: [
+  //     ,
+  //   ],
+  //   when: {
+  //     show: () => {
+  //       console.log("show step");
+  //     },
+  //     hide: () => {
+  //       console.log("hide step");
+  //     },
+  //   },
+  //   buttons: [
+  //     {
+  //       classes: "max-index button-primary",
+  //       text: "Back",
+  //       type: "back",
+  //     },
+  //     {
+  //       classes: "max-index button-primary",
+  //       text: "Next",
+  //       type: "next",
+  //     },
+  //   ],
+  // },
   {
     id: "tourSkills",
     attachTo: { element: "#tourSkills", on: "auto" as const },
@@ -98,7 +100,7 @@ const tourSteps = [
     },
     title: "The Skills",
     text: [
-      "I can create responsive websites and apps using those skills.<br/>Check them out by clicking them!",
+      "I can create responsive websites and apps using those skills.<br/>Check them out by clicking them! </br> Have fun checking out this website!",
     ],
     when: {
       show: () => {
@@ -116,80 +118,80 @@ const tourSteps = [
       },
       {
         classes: "max-index button-primary",
-        text: "Next",
+        text: "Finish",
         type: "next",
       },
     ],
   },
-  {
-    id: "tourLevelMeUp",
-    attachTo: { element: "#tourStart", on: "auto" as const },
-    beforeShowPromise: function () {
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          window.scrollTo(0, 0);
-          resolve(true);
-        }, 500);
-      });
-    },
-    useModalOverlay: false,
-    highlightClass: "highlight",
-    scrollTo: false,
-    cancelIcon: {
-      enabled: true,
-    },
-    title: "Want to know more?",
-    text: ["Click on Me and LEVEL ME UP! 😄"],
-    when: {
-      show: () => {
-        console.log("show step");
-      },
-      hide: () => {
-        console.log("hide step");
-      },
-    },
-    buttons: [
-      {
-        classes: "max-index button-primary",
-        text: "Back",
-        type: "back",
-      },
-    ],
-  },
-  {
-    id: "tourButton",
-    attachTo: { element: "#tourButton", on: "auto" as const },
-    beforeShowPromise: function () {
-      return new Promise(function (resolve) {
-        setTimeout(function () {
-          window.scrollTo(0, 0);
-          resolve(true);
-        }, 500);
-      });
-    },
-    useModalOverlay: false,
-    highlightClass: "highlight",
-    scrollTo: false,
-    cancelIcon: {
-      enabled: true,
-    },
-    title: " ",
-    text: ["Push The Button!"],
-    when: {
-      show: () => {
-        console.log("show step");
-      },
-      hide: () => {
-        console.log("hide step");
-      },
-    },
-    buttons: [
-      {
-        classes: "max-index button-primary",
-        text: "Back",
-        type: "back",
-      },
-    ],
-  },
+  // {
+  //   id: "tourLevelMeUp",
+  //   attachTo: { element: "#tourStart", on: "auto" as const },
+  //   beforeShowPromise: function () {
+  //     return new Promise(function (resolve) {
+  //       setTimeout(function () {
+  //         window.scrollTo(0, 0);
+  //         resolve(true);
+  //       }, 500);
+  //     });
+  //   },
+  //   useModalOverlay: false,
+  //   highlightClass: "highlight",
+  //   scrollTo: false,
+  //   cancelIcon: {
+  //     enabled: true,
+  //   },
+  //   title: "Want to know more?",
+  //   text: ["Click on Me and LEVEL ME UP! 😄"],
+  //   when: {
+  //     show: () => {
+  //       console.log("show step");
+  //     },
+  //     hide: () => {
+  //       console.log("hide step");
+  //     },
+  //   },
+  //   buttons: [
+  //     {
+  //       classes: "max-index button-primary",
+  //       text: "Back",
+  //       type: "back",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: "tourButton",
+  //   attachTo: { element: "#tourButton", on: "auto" as const },
+  //   beforeShowPromise: function () {
+  //     return new Promise(function (resolve) {
+  //       setTimeout(function () {
+  //         window.scrollTo(0, 0);
+  //         resolve(true);
+  //       }, 500);
+  //     });
+  //   },
+  //   useModalOverlay: false,
+  //   highlightClass: "highlight",
+  //   scrollTo: false,
+  //   cancelIcon: {
+  //     enabled: true,
+  //   },
+  //   title: " ",
+  //   text: ["Push The Button!"],
+  //   when: {
+  //     show: () => {
+  //       console.log("show step");
+  //     },
+  //     hide: () => {
+  //       console.log("hide step");
+  //     },
+  //   },
+  //   buttons: [
+  //     {
+  //       classes: "max-index button-primary",
+  //       text: "Back",
+  //       type: "back",
+  //     },
+  //   ],
+  // },
 ];
 export default tourSteps;
